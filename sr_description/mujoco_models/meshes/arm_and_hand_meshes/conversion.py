94160bef721894d35c253f399cc14d46b4016ff1
Ethan Fowler <ethan@shadowrobot.com>
2018-11-14 13:58:08 +0000

    Lint fixes.

--- sr_description/mujoco_models/meshes/arm_and_hand_meshes/conversion.py
+++ sr_description/mujoco_models/meshes/arm_and_hand_meshes/conversion.py
@@ -6,7 +6,8 @@ import os
 
 sr_description_path = '/home/user/projects/shadow_robot/base_deps/src/sr_common/sr_description'
 
-file_names = ['forearm', 'forearm_muscle', 'forearm_muscle_disk', 'forearm_lite', 'wrist', 'palm', 'knuckle', 'lfmetacarpal', 'F1', 'F2', 'F3', 'TH1_z', 'TH2_z', 'TH3_z']
+file_names = ['forearm', 'forearm_muscle', 'forearm_muscle_disk', 'forearm_lite', 'wrist', 'palm', 'knuckle',
+              'lfmetacarpal', 'F1', 'F2', 'F3', 'TH1_z', 'TH2_z', 'TH3_z']
 
 for file_name in file_names:
     source_file_name = '{0}/hand/model/{1}.dae'.format(sr_description_path, file_name)
@@ -16,10 +17,10 @@ for file_name in file_names:
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete()
 
-    bpy.ops.wm.collada_import(filepath=source_file_name) # change this line
+    bpy.ops.wm.collada_import(filepath=source_file_name)  # change this line
 
     bpy.ops.object.select_all(action='SELECT')
-    
+
     bpy.ops.transform.rotate(value=4.71238898038, axis=(1.0, 0, 0))
 
     bpy.ops.export_mesh.stl(filepath=dest_file_name)
