368d8302c83bf4e5d6281852fbdcc98dc5f7031c
maxzieba <max@shadowrobot.com>
2021-10-26 08:06:28 +0000

    Applying changes from https://github.com/ubi-agni/sr_common/pull/2

--- sr_description/test/test_hands.sh
+++ sr_description/test/test_hands.sh
@@ -3,7 +3,4 @@
 
 source ./test_models.sh
 
-launch sr_hand hand_type:="hand_e" hand_version:="E3M5" side:="right" fingers:="th,ff,lf" tip_sensors:="pst"
-launch sr_hand hand_type:="hand_e" hand_version:="E3M5" side:="left" fingers:="th,ff,lf" tip_sensors:="pst"
-launch sr_hand hand_type:="hand_e" hand_version:="E3M5" side:="right" fingers:="th,ff,lf" tip_sensors:="th=bt_2p,ff=bt_sp,mf=pst,rf=pst,lf=pst"
-launch sr_hand
+launch sr_hand hand_type:="hand_ex" hand_version:="E3M5" side:="right" fingers:="th,ff,lf" tip_sensors:="pst"
